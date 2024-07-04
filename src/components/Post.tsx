a83190b398cdf6184ea8d5b9548a1e9582f80893
Zer0xNu11 <zerfnult@gmail.com>
2024-07-05 01:32:09 +0900

    change rdm

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -11,7 +11,7 @@ const Post:React.FC<PostProps> = ({post}) => {
     <div className="flex items-center mb-2">
       <img
         className="w-10 h-10 rounded-full mr-2"
-        src="https://via.placeholder.com/150"
+        src="/images/placeholder"
         alt="User Avatar"
       />
       <div>
