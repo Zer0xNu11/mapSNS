fe3965875b89a4b15fd3c4ce67e686fd74ac0061
Zer0xNu11 <zerfnult@gmail.com>
2024-07-12 01:14:07 +0900

    img 2 Image

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -2,6 +2,7 @@ import { PostType } from "@/types";
 import { auth } from "@/auth";
 import { prismadb } from "@/globals/db";
 import { PostLikeIcon } from "./PostLikeIcon";
+import Image from "next/image";
 
 export interface PostProps {
   post: PostType;
@@ -75,7 +76,7 @@ const Post: React.FC<PostProps> = async ({post}) => {
     <div className="bg-white shadow-md rounded p-4 mb-4">
       <div className="mb-4">
         <div className="flex items-center mb-2">
-          <img
+          <Image
             className="w-10 h-10 rounded-full mr-2"
             src="images/placeholder.png"
             alt="User Avatar"
