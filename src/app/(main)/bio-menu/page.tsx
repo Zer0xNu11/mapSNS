52f6934dedf8e4d7142cff20606318119d468c6e
Zer0xNu11 <zerfnult@gmail.com>
2024-07-24 09:45:05 +0900

    rep take3

--- src/app/(main)/bio-menu/page.tsx
+++ src/app/(main)/bio-menu/page.tsx
@@ -1,11 +1,13 @@
 // import Map from '@/components/map/Map';
-import { prismadb } from '@/globals/db';
-import dynamic from 'next/dynamic';
-import React from 'react'
+import { prismadb } from "@/globals/db";
+import dynamic from "next/dynamic";
+import React from "react";
 
 async function getBook() {
   //生SQLで時系列順にpostを取得 ->json配列 ->>json文字列
-  const posts = await prismadb.$queryRaw<Array<{ id: string, content: string, coordinates: [number, number] }>>`
+  const posts = await prismadb.$queryRaw<
+    Array<{ id: string; content: string; coordinates: [number, number] }>
+  >`
     SELECT id, content, ST_AsGeoJSON(location)::json->'coordinates' as coordinates
     FROM "Post"
     WHERE location IS NOT NULL
@@ -13,42 +15,47 @@ async function getBook() {
   `;
 
   // 座標データを[latitude, longitude]の形式に変換
-  const postsForLeaflet = posts.map(post => ({
+  const postsForLeaflet = posts.map((post) => ({
     ...post,
-    coordinates: [post.coordinates[1], post.coordinates[0]] as [number, number]
+    coordinates: [post.coordinates[1], post.coordinates[0]] as [number, number],
   }));
 
   // Polyline用の座標配列
-  const polylineCoordinates = postsForLeaflet.map(post => post.coordinates);
-  console.log({posts: postsForLeaflet,
-    polylineCoordinates : polylineCoordinates})
+  const polylineCoordinates = postsForLeaflet.map((post) => post.coordinates);
+  console.log({
+    posts: postsForLeaflet,
+    polylineCoordinates: polylineCoordinates,
+  });
   return {
     posts: postsForLeaflet,
-    polylineCoordinates
+    polylineCoordinates,
   };
 }
 
+const Map = dynamic(() => import("@/components/map/Map"), {
+  loading: () => <p>map is loading</p>,
+  ssr: false,
+});
 
 const MapPage = async () => {
-  const Map = React.useMemo(
-    () =>
-      dynamic(() => import("@/components/map/Map"), { //SSR停止　window error防止
-        loading: () => <p>map is loading</p>,
-        ssr: false,
-      }),
-    []
-  );
+  // const Map = React.useMemo(
+  //   () =>
+  //     dynamic(() => import("@/components/map/Map"), { //SSR停止　window error防止
+  //       loading: () => <p>map is loading</p>,
+  //       ssr: false,
+  //     }),
+  //   []
+  // );
 
   const { posts, polylineCoordinates } = await getBook();
 
-
   return (
-    <div className='w-full h-[100vh]'>
-    <div className='w-[80%] h-[80vh]'>
-      <Map posts={posts} polylineCoordinates={polylineCoordinates}/>
+    <div className="w-full h-[100vh]">
+      <div className="w-[80%] h-[80vh]">
+        <Map posts={posts} polylineCoordinates={polylineCoordinates} />
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default MapPage
+export default MapPage;
