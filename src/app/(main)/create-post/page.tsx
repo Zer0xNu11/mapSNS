c5bec848246471760b195321850d87139e883f8a
Zer0xNu11 <zerfnult@gmail.com>
2024-07-24 12:39:25 +0900

    rep take7

--- src/app/(main)/create-post/page.tsx
+++ src/app/(main)/create-post/page.tsx
@@ -3,16 +3,17 @@
 import dynamic from "next/dynamic";
 import React from "react";
 
-const PostForm = React.useMemo(
-  () =>
-    dynamic(() => import("@/components/Post/PostForm"), { //SSR停止　window error防止
-      loading: () => <p>map is loading</p>,
-      ssr: false,
-    }),
-  []
-);
+
 
 const CreatePost = () => {
+  const PostForm = React.useMemo(
+    () =>
+      dynamic(() => import("@/components/Post/PostForm"), { //SSR停止　window error防止
+        loading: () => <p>map is loading</p>,
+        ssr: false,
+      }),
+    []
+  );
   
   return (
     <>
