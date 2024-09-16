4942f9e6cbafcdb596b73bca59a228567762ff4b
Zer0xNu11 <zerfnult@gmail.com>
2024-09-17 08:29:16 +0900

    retry2

--- src/app/(main)/home/share/page.tsx
+++ src/app/(main)/home/share/page.tsx
@@ -1,15 +1,15 @@
-// import ListFromSort from '@/components/map/editMap/ListFromSort'
-// import React from 'react'
+import ListFromSort from '@/components/map/editMap/ListFromSort'
+import React from 'react'
 
-// const SharePage = () => {
-//   return (
-//     <>
-//     <div>SharePage</div>
-//     <ListFromSort/>
-//     </>
-//   )
-// }
+const SharePage = () => {
+  return (
+    <>
+    <div>SharePage</div>
+    {/* <ListFromSort/> */}
+    </>
+  )
+}
 
-// export default SharePage
+export default SharePage
 
 
