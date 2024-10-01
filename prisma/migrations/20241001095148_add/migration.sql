d519f0e87dabcb9f3d0ef25b5e0a038ce1d6f809
Zer0xNu11 <zerfnult@gmail.com>
2024-10-01 19:07:27 +0900

    cascade database

---
+++ prisma/migrations/20241001095148_add/migration.sql
@@ -0,0 +1,14 @@
+-- DropForeignKey
+ALTER TABLE "Like" DROP CONSTRAINT "Like_postId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Like" DROP CONSTRAINT "Like_userId_fkey";
+
+-- AlterTable
+ALTER TABLE "PlanPoint" ADD COLUMN     "memo" TEXT DEFAULT '';
+
+-- AddForeignKey
+ALTER TABLE "Like" ADD CONSTRAINT "Like_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Like" ADD CONSTRAINT "Like_postId_fkey" FOREIGN KEY ("postId") REFERENCES "Post"("id") ON DELETE CASCADE ON UPDATE CASCADE;
