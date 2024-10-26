32783d4c4397e83c3dd9e054f22570a8bd69a20a
Zer0xNu11 <zerfnult@gmail.com>
2024-10-26 12:57:27 +0900

    pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+// const nextConfig = {};
 
-export default nextConfig;
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development'
+})
+
+const nextConfig = {
+  // App Routerの設定
+  reactStrictMode: true,
+}
+
+export default withPWA(nextConfig);
