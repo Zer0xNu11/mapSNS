0f6deeef552d2153536c893169ec669a09fc42e5
Zer0xNu11 <zerfnult@gmail.com>
2024-11-13 07:19:32 +0900

    Revert "PWA3"
    This reverts commit 97b41975f84101371847cc4047dd2fb5de25fec2.

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,15 @@
-import withPWA from 'next-pwa'
+import nextPWA from 'next-pwa'
 
 /** @type {import('next').NextConfig} */
 
-const nextPWA = withPWA ({
+const withPWA = nextPWA ({
   dest: "public",
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === "development",
 });
 
-const nextConfig = nextPWA({
+const nextConfig = withPWA({
   reactStrictMode: true,
 });
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata, Viewport } from "next";
+import type { Metadata } from "next";
 import { Noto_Sans_JP } from "next/font/google";
 import "./globals.css";
 
@@ -9,6 +9,7 @@ export const metadata: Metadata = {
   title: "Putterec",
   description: "Location-based SNS application for solo travelers",
   manifest: '/manifest.json',
+  themeColor: '#000000',
   icons: {
     icon: [
       { url: '/icon.png', sizes: '192x192', type: 'image/png' },
@@ -16,14 +17,12 @@ export const metadata: Metadata = {
     ],
     apple: '/apple-icon.png',
   },
+  viewport: {
+    width: 'device-width',
+    initialScale: 1,
+    maximumScale: 1,
+  },
 };
-
-export const   viewport: Viewport = {
-  themeColor: '#000000',
-  width: 'device-width',
-  initialScale: 1,
-  maximumScale: 1,
-}
 
 export default async function RootLayout({
   children,
